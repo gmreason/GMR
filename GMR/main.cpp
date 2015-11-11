798cd1ec1c48beb12726b6172bc7be3654508057
gmreason <453948208@qq.com>
2015-11-11 21:04:57 +0800

    three
    顺序输出加动态存储分配

--- GMR/main.cpp
+++ GMR/main.cpp
@@ -11,12 +11,28 @@
 int main(int argc, const char * argv[]) {
     // insert code here...
     //std::cout << "Hello, World!\n";
-    int a[3];
-    int *p;
-    a[0]=11;
-    a[1]=22;
-    a[2]=33;
-    p=a;
-    printf("&a[0]=%x &a[1]=%x &a[2]=%x a=%x *p=%x",&a[0],&a[1],&a[2],a,*p);
+    int *a;
+    int i,j,temp;
+    int N;
+    scanf("%d",&N);
+    a=(int *)malloc(N=4);
+    if(a==0)
+        exit(0);
+    for(i=0;i<N;i++)
+        scanf("%d",&a[i]);
+    for(i=0;i<N-1;i++)
+    {
+        for(j=0;j<N-1-i;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+        }
+    }
+    for(i=0;i<N;i++)
+        printf("%d\n",a[i]);
     return 0;
 }
