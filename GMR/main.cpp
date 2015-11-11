e898f3757b8b83cd6885e5c586ef427e5fc7edee
gmreason <453948208@qq.com>
2015-11-11 20:41:04 +0800

    Initial Commit

---
+++ GMR/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GMR
+//
+//  Created by gmr on 15/11/11.
+//  Copyright © 2015年 gmr. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
