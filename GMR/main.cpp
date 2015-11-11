8c64d11a74c86f47203eb32559792b7a23fe60d4
gmreason <453948208@qq.com>
2015-11-11 20:51:09 +0800

    two
    输出指针变量、整形变量的地址

--- GMR/main.cpp
+++ GMR/main.cpp
@@ -11,9 +11,12 @@
 int main(int argc, const char * argv[]) {
     // insert code here...
     //std::cout << "Hello, World!\n";
-    int a,*p;
-    a=100;
-    p=&a;
-    printf("a=%x &a=%x p=%x &p=%x *p=%x\n",a,&a,p,&p,*p);
+    int a[3];
+    int *p;
+    a[0]=11;
+    a[1]=22;
+    a[2]=33;
+    p=a;
+    printf("&a[0]=%x &a[1]=%x &a[2]=%x a=%x *p=%x",&a[0],&a[1],&a[2],a,*p);
     return 0;
 }
