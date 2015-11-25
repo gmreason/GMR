b9cf1b15522e81fa28bd0f0b14788c2e296b8037
gmreason <453948208@qq.com>
2015-11-25 19:18:16 +0800

    output student's information
    输出学生姓名性别年龄学号地址

--- GMR/main.cpp
+++ GMR/main.cpp
@@ -6,57 +6,46 @@
 //  Copyright © 2015年 gmr. All rights reserved.
 //
 
-#include<iostream>
+#include<iostream> 
 using namespace std;
-class Complex                                                        //定义Complex类
+class Student
 {
-public:  Complex();                                                  //默认构造函数
-    friend Complex operator+(Complex &,Complex &);                   //重载运算符“+”
-    void input();                                                    //输入数据函数
-    void display();                                                  //输出数据函数
-private:
-    int com[2][3];
-};
-Complex::Complex()                                                   //定义构造函数
-{
-    for(int i=0;i<2;i++)
-        for(int j=0;j<3;j++)
-            com[i][j]=0;
-}
-Complex operator+(Complex &a,Complex &b)                             //定义重载运算符“+”函数
-{
-    Complex c;
-    for(int i=0;i<2;i++)
-        for(int j=0;j<3;j++)
-        {c.com[i][j]=a.com[i][j]+b.com[i][j];}
-    return c;
-}
-void Complex::input()                                                //定义输入数据函数
-{
-    cout<<"input value of Complex:"<<endl;
-    for(int i=0;i<2;i++)
-        for(int j=0;j<3;j++)
-            cin>>com[i][j];
-}
-void Complex::display()                                              //定义输出数据函数
-{
-    for (int i=0;i<2;i++)
+    public:
+    void get_value()
+    {cin>>num>>name>>sex;}
+    void display()
     {
-        for(int j=0;j<3;j++)
-        {cout<<com[i][j]<<" ";}
-        cout<<endl;
-    }
-}   int main()
+        cout<<"num: "<<num<<endl;
+        cout<<"name: "<<name<<endl;
+        cout<<"sex: "<<sex<<endl;}
+    private:
+    int num;
+    char name[10];
+    char sex;
+};
+class Student1:
+public Student
 {
-    Complex a,b,c;
-    a.input();
-    b.input();
-    cout<<endl<<"Complex a:"<<endl;
-    a.display();
-    cout<<endl<<"Complex b:"<<endl;
-    b.display();
-    c=a+b;                                                           //用重载运算符“+”实现两个矩阵相加
-    cout<<endl<<"Complex c = Complex a + Complex b :"<<endl;
-    c.display();
+    public:
+    void get_value_1()
+    {
+        get_value();
+        cin>>age>>addr;
+    }
+    void display_1()
+    {
+        cout<<"age:"<<age<<endl;          //引用派生类的私有成员
+        cout<<"address:"<<addr<<endl;     //引用派生类的私有成员
+    }
+    private:
+    int age;
+    char addr[30];
+};
+int main()
+{
+    Student1 stud1;
+    stud1.get_value_1();
+    stud1.display();
+    stud1.display_1();
     return 0;
 }
