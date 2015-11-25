500f79f30c37070f76491e4d77167530860aa7c4
gmreason <453948208@qq.com>
2015-11-25 21:16:28 +0800

    output changed student's num score
    1、定义一个 Student类
    2、声明public（公有成员）和private（私有成员）
    3、public下有void change（改变函数）和void display（）（显示、输出函数）
    4、private下有整数num（学号）和浮点数score（成绩）
    5、主函数：
    int main()
    {
        Student stu(101,78.5);
        void fun(Student&);
        fun(stu);
        return 0;
    }
    6、定义一个fun函数
    7、输出学生的学号和改变后的成绩

--- GMR/main.cpp
+++ GMR/main.cpp
@@ -6,46 +6,29 @@
 //  Copyright © 2015年 gmr. All rights reserved.
 //
 
-#include<iostream> 
+#include<iostream>
 using namespace std;
 class Student
 {
-    public:
-    void get_value()
-    {cin>>num>>name>>sex;}
-    void display()
-    {
-        cout<<"num: "<<num<<endl;
-        cout<<"name: "<<name<<endl;
-        cout<<"sex: "<<sex<<endl;}
-    private:
+public:
+    Student(int n,float s):num(n),score(s){}
+    void change(int n,float s) {num=n,score=s;}
+    void display()const {cout<<num<<""<<score<<endl;}
+private:
     int num;
-    char name[10];
-    char sex;
+    float score;
 };
-class Student1:
-public Student
-{
-    public:
-    void get_value_1()
-    {
-        get_value();
-        cin>>age>>addr;
-    }
-    void display_1()
-    {
-        cout<<"age:"<<age<<endl;          //引用派生类的私有成员
-        cout<<"address:"<<addr<<endl;     //引用派生类的私有成员
-    }
-    private:
-    int age;
-    char addr[30];
-};
+
 int main()
 {
-    Student1 stud1;
-    stud1.get_value_1();
-    stud1.display();
-    stud1.display_1();
+    Student stu(101,78.5);
+    void fun(Student&);
+    fun(stu);
     return 0;
+}
+void fun(Student &stu)
+{
+    stu.display();
+    stu.change(101,80.5);
+    stu.display();
 }
